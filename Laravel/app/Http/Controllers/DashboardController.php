2c8f840966acc4f4cdbbaae56073457d4ed48fff
Collins Tanki <collinstanki@gmail.com>
2021-07-22 15:51:40 +0300

    added post model

--- Laravel/app/Http/Controllers/DashboardController.php
+++ Laravel/app/Http/Controllers/DashboardController.php
@@ -12,8 +12,10 @@ class DashboardController extends Controller
     }
    public function index()
    {
+       dd(auth()->user()->posts);
        
        return view('dashboard');
+
    }
 
    
--- Laravel/resources/views/posts/index.blade.php
+++ Laravel/resources/views/posts/index.blade.php
@@ -2,8 +2,34 @@
 @section('content')
 <div class="flex justify-center">
     <div class="w-8/12 bg-white p-6 rounded-lg">
-    Post
+   <form action="{{route('posts')}}" method="post" class="mb-4">
+        @csrf
+   <div class="mb-4">
+   <label for="body" class="sr-only">Body</label>
+   <textarea name="body" id="body" cols="30" rows="4" class="bg-gray-100 border-2 w-full p-4 runded-lg @error('body') border-red @enderror}" placeholder="Post Something!!"></textarea>
 
+   @error('body')
+   <div class="text-red-500 mt-2 text-sm">
+      {{$message}} 
+      </div>
+    @enderror
+    </div>
+        <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded font-medium">Post</button>
+    
+    </form>
+    @if ($posts->count())
+        @foreach ($posts as $post)
+            <div class="mb-4 ">
+            <a href="" class="font-bold p-2">{{$post->user->name}}</a><span class="text-gray-600 text-sm">{{$post->created_at->diffForHumans()}}</span>
+            <p class="mb-2">{{$post->body}}</p>               
+                </div>
+        @endforeach
+
+    @else
+    <p>There are no posts</p>
+
+    @endif
+ 
     </div>
 
 </div>
